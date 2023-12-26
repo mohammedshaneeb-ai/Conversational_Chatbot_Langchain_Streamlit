e269721a267ea9bb525a3be0cfdda4879d172914
mohammedshaneeb-ai <shaneebkottakkal@gmail.com>
2023-12-26 17:16:44 +0530

    openai version changed

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import pinecone
-import openai
-import streamlit as st
+from openai import OpenAI
 import os
+
+client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
-openai.api_key = os.environ.get('OPENAI_API_KEY')
 PineCone_KEY = os.environ.get('PineCone_KEY')
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
@@ -22,15 +23,13 @@ def find_match(input):
     return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
 
 def query_refiner(conversation, query):
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt = f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base. \n\nConversation LOG: \n{conversation}\n\nQuery:{query}\n\n Refined Query: ",
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
+    response = client.completions.create(model="text-davinci-003",
+    prompt = f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base. \n\nConversation LOG: \n{conversation}\n\nQuery:{query}\n\n Refined Query: ",
+    temperature=0.7,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0)
     return response['choices'][0]['text']
 
 def get_conversation_string():
